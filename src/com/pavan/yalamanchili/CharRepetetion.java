f8bd72422910152668b03f9c4eb814090a872e9c
Pavan Yalamanchili <ypavanchowdary@outlook.com>
2017-06-29 17:28:56 -0400

    Created a new class file with main function

---
+++ src/com/pavan/yalamanchili/CharRepetetion.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.pavan.yalamanchili;
+
+/**
+ * @author ypava
+ *
+ */
+public class CharRepetetion {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

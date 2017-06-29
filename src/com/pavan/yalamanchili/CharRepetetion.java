dd33d910a12257422b7d2cc92db7db7ce6b88295
Pavan Yalamanchili <ypavanchowdary@outlook.com>
2017-06-29 17:37:29 -0400

    Main Logic is updated and it's working fine

--- src/com/pavan/yalamanchili/CharRepetetion.java
+++ src/com/pavan/yalamanchili/CharRepetetion.java
@@ -27,10 +27,24 @@ public class CharRepetetion {
 
 public static void getCount(String name) {
         
-       
+	  Map<Character, Integer> names = 
+              new LinkedHashMap<Character, Integer>();
+      for (int i = 0; i < name.length(); i++) {
+          char c = name.charAt(i);
+          Integer count = names.get(c);
+          if (count == null) {
+              count = 0;
+          }
+          names.put(c, count + 1);
+      }
+      Set<Character> a = names.keySet();
+      for (Character t : a) {
+          if (!t.equals(' '))
+              System.out.println(t + " Ocurred " 
+          + names.get(t) + " times");
 
         }
     }
-
+}
 
 

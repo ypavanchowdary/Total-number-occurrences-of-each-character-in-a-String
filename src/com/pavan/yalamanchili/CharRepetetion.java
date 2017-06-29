e9b8035266602acc5582d98a0739abe1671afc88
Pavan Yalamanchili <ypavanchowdary@outlook.com>
2017-06-29 17:32:50 -0400

    Methods Declared

--- src/com/pavan/yalamanchili/CharRepetetion.java
+++ src/com/pavan/yalamanchili/CharRepetetion.java
@@ -19,7 +19,15 @@ public class CharRepetetion {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		String str = "Pavan Yalamanchili";
+        getCount(str);
 
+	}
+
+	private static void getCount(String str) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
